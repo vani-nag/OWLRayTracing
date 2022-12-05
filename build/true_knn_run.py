300fa9b8575b449f2114ae0b310c4104a6fafc71
Vani Nagarajan <nagara16@tgrogers-gpu02.ecn.purdue.edu>
2022-12-05 05:50:44 -0500

    script to run TrueKNN

--- build/true_knn_run.py
+++ build/true_knn_run.py
@@ -5,24 +5,26 @@ import os
 #radii = [0.001, 0.002, 0.004, 0.008, 0.01, 0.02, 0.04, 0.08, 0.1]
 #radii_porto = [0.000009, 0.00003, 0.00011, 0.00012, 0.00014, 0.00025, 0.00026, 0.0003, 0.026]
 
-#knn-datasize
+#We test TrueKNN with k=5 and k=sqrt(DatasetSize). knn_datasize is a dictonary with DatasetSize as key
 knn_datasize = {}
-'''
 knn_datasize[100000] = [5,316]
 knn_datasize[200000] = [5,447]
-knn_datasize[400000] = [5, 660]
-knn_datasize[800000] = [5, 894]
-'''
-knn_datasize[100000] = [316]
+knn_datasize[400000] = [5,660]
+knn_datasize[800000] = [5,894]
+
+'''knn_datasize[100000] = [316]
 knn_datasize[200000] = [447]
 knn_datasize[400000] = [660]
-knn_datasize[800000] = [894]
+knn_datasize[800000] = [894]'''
 
+'''
+radii_<Dataset> is a dictonary holding the start radius for trueKNN
+This needs to be filled in using output of owl/samples/cmdline/s01-simpleTriangles/testing/random_sample.py
+'''
 #Porto
 radii_porto = {}
 radii_porto[100000] = [0.8]
 radii_porto[200000] = [0.5]
-
 radii_porto[800000] = [0.8]
 '''
 radii_porto[100000] = [0.000009, 0.00007, 0.0001, 0.0002, 0.0004, 0.0005, 0.022]
@@ -56,8 +58,10 @@ radii_kitti[800000] = [0.08]
 radii_uniform_dist = {}
 radii_uniform_dist[400000] = [0.05, 0.2]
 
-#sizes = [100000, 200000, 400000, 800000]
-sizes = [100000, 200000, 800000]
+#Holds dataset sizes we are experimenting with. Can be increased to millions?
+sizes = [100000, 200000, 400000, 800000]
+
+
 
 os.system("make sample06-rtow-mixedGeometries")
 
@@ -123,7 +127,7 @@ for size in sizes:
 				f.close()
 			mean = float(sum(data))/len(data) if len(data) > 0 else float('nan')
 			print("Radius = "+str(radius)+" Avg Time = "+str(mean))
-'''
+
 
 #Porto
 for size in sizes:
@@ -143,6 +147,35 @@ for size in sizes:
 #Average
 for size in sizes:
 	file_path = "/home/min/a/nagara16/Downloads/owl/build/MyResults/TrueKNN/Porto/Points_"+str(size)+"/"+str(k)+"knn"
+	print("\n Dataset Size = "+str(size))
+	
+	for radius in radii_porto[size]:
+		filename = file_path + "/start_radius_"+str(radius)
+		with open(filename,'r') as f:
+			data = [float(line.rstrip()) for line in f.readlines()]
+			f.close()
+		mean = float(sum(data))/len(data) if len(data) > 0 else float('nan')
+		print("Radius = "+str(radius)+" Avg Time = "+str(mean))
+
+'''
+#UniformDist
+for size in sizes:
+	for k in knn_datasize[size]:
+		file_path = "/home/min/a/nagara16/Downloads/owl/build/MyResults/TrueKNN/UniformDist/Points_"+str(size)+"/"+str(k)+"knn"
+		os.makedirs(file_path, exist_ok = True)
+		output_path = file_path + "/Outputs/"
+		os.makedirs(output_path, exist_ok = True)
+		
+		for radius in radii_porto[size]:
+			filename = file_path + "/start_radius_"+str(radius)
+			for i in range(0,1):
+				cmd = "./sample06-rtow-mixedGeometries /home/min/a/nagara16/Downloads/owl/samples/cmdline/s01-simpleTriangles/testing/porto.csv "+str(size)+" "+str(radius)+" "+str(k)+" "+filename+" >> "+output_path+"/start_radius_"+str(radius)+"_output"
+				os.system(cmd)
+
+
+#Average
+for size in sizes:
+	file_path = "/home/min/a/nagara16/Downloads/owl/build/MyResults/TrueKNN/UniformDist/Points_"+str(size)+"/"+str(k)+"knn"
 	print("\n Dataset Size = "+str(size))
 	
 	for radius in radii_porto[size]:
